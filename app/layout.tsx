6c4a2a9c1f652857a6ecee33cef267ccd380b700
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-11-20 18:21:29 +0700

    feat(viewport): enhance viewport settings with additional properties

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,11 @@ const geistMono = localFont({
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  minimumScale: 1,
   maximumScale: 1,
+  viewportFit: 'cover',
+  colorScheme: 'light dark',
+  height: 'device-height',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#ffffff' },
     { media: '(prefers-color-scheme: dark)', color: '#09090b' }
