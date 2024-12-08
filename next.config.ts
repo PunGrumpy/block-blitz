add5cae9462c8c0c0d8b385fb02ddc0f8e4c0dda
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-12-09 02:21:01 +0700

    refactor(next-config): change env

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
-    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
-    KV_REST_API_URL: process.env.KV_REST_API_URL,
-    KV_URL: process.env.KV_URL,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     ADMIN_SECRET: process.env.ADMIN_SECRET,
     SCORE_SECRET: process.env.SCORE_SECRET
   }
